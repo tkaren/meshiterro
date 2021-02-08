e101ecfcca8cc5cd6abda1bebf4abf7740e41ec6
tsunodakaren <k6432286@gmail.com>
2021-02-08 11:14:52 +0000

    [Add] 投稿機能]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5bee647aae401d6af5bae47e5672069fe1767f18c149dfbfd122d38adc90fe690f70e17813aef5416da05034398516387f631e360d08666a02bbc213365773ae'
